6aa1c8ba3a7e826fbeaaa510e58aed8de1a563a6
Jesse B. Hannah <jesse@jbhannah.net>
2013-02-13 22:17:49 -0700

    Initial commit

---
+++ lib/has_swiftype/version.rb
@@ -0,0 +1,3 @@
+module HasSwiftype
+  VERSION = "0.0.1"
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'b80d4766a2d11bfc6a9411451c0fb845811f7c66ca7e201e78cfbf9e57f189f98ba71aaedb30997c1dbb3465d6c5647ddfd47673df01df67ccbbce3940a449f3'
